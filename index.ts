831e1411940e46a006d9dff92c496bde32d4f39c
oneralon <17377013+oneralon@users.noreply.github.com>
2025-03-21 13:04:56 +0300

    [DEV-2198] Move common loaders

--- index.ts
+++ index.ts
@@ -10,7 +10,76 @@ import {
 import { APP_INTERCEPTOR, ModuleRef, ContextIdFactory } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import DataLoader from 'dataloader';
-import { Observable } from 'rxjs';
+import { ObjectId } from 'mongodb';
+import { Types } from 'mongoose';
+
+export class ObjectIdMap<K extends Id, V> implements Omit<Map<K, V>, 'get'> {
+  private map: Map<string, [K, V]>;
+
+  constructor(entries: Array<[K, V]> = []) {
+    this.map = new Map(entries.map(([k, v]) => [k.toString(), [k, v]]));
+  }
+
+  public get(key?: K) {
+    return key ? (this.map.get(key.toString()) ?? [])[1] : undefined;
+  }
+
+  public set(key: K, value: V) {
+    this.map.set(key.toString(), [key, value]);
+
+    return this;
+  }
+
+  public clear() {
+    this.map.clear();
+  }
+
+  public delete(key: K) {
+    return this.map.delete(key.toString());
+  }
+
+  public has(key: K) {
+    return this.map.has(key.toString());
+  }
+
+  public get size() {
+    return this.map.size;
+  }
+
+  public keys() {
+    return Array.from(this.map.values()).map(([key]) => key).values();
+  }
+
+  public values() {
+    return Array.from(this.map.values()).map(([, value]) => value).values();
+  }
+
+  public entries() {
+    return this.map.values();
+  }
+
+  public forEach(callbackfn: (value: V, key: K, map: Map<K, V>) => void, thisArg?: any) {
+    new Map(this.map.values()).forEach(callbackfn.bind(thisArg ?? this));
+  }
+
+  public [Symbol.iterator](): IterableIterator<[K, V]> {
+    return this.entries();
+  }
+
+  public get [Symbol.toStringTag]() {
+    return this.constructor.name;
+  }
+
+  public [Symbol.for('nodejs.util.inspect.custom')]() {
+    return new Map(this.map.values());
+  }
+
+  public toJSON() {
+    return Array
+      .from(this.entries())
+      .reduce((res, [key, val]) => ({ ...res, [key.toString()]: val }), {});
+  }
+}
 
 /**
  * This interface will be used to generate the initial data loader.                
@@ -39,7 +108,7 @@ export class DataLoaderInterceptor implements NestInterceptor {
   /**
    * @inheritdoc
    */
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+  intercept(context: ExecutionContext, next: CallHandler) {
     const graphqlExecutionContext = GqlExecutionContext.create(context);
     const ctx = graphqlExecutionContext.getContext();
 
@@ -62,6 +131,39 @@ export class DataLoaderInterceptor implements NestInterceptor {
       };
     }
     return next.handle();
+  }
+}
+
+@Injectable()
+export abstract class OrderedDataLoader<ID, Type> implements NestDataLoader<ID, Type> {
+  public abstract query(keys: Array<ID>): Promise<Array<Type>>;
+
+  public generateDataLoader() {
+    return new DataLoader<ID, Type>(async (keys) => {
+      return this.query(keys as Array<ID>);
+    });
+  }
+}
+
+type Id = ObjectId | Types.ObjectId;
+
+export abstract class MongodbDataLoader<ID extends Id, Type extends { _id: Id }, ReturnType>
+  extends OrderedDataLoader<ID, ReturnType>
+  implements NestDataLoader<ID, ReturnType> {
+  protected checkAndOrder(keys: Array<ID>, items: Array<Type> | ObjectIdMap<ID, Type>): Array<Type> {
+    const itemsMap = items instanceof ObjectIdMap
+      ? items
+      : new ObjectIdMap<ID, Type>(items.map(item => [item._id as ID, item]));
+
+    const result = keys.map(id => itemsMap.get(id));
+
+    if (result.some(item => !item)) {
+      const missed = keys.filter(id => !itemsMap.get(id));
+
+      throw new Error(`Could not find ${ missed.join(', ') } for ${ this.constructor.name }`);
+    }
+
+    return result as Array<Type>;
   }
 }
 
